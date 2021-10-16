2665b1bc47df5a23f9a4e2bcdeb47778bdc3c6da
FranMedic <m.francisca.medico@gmail.com>
2021-10-16 11:05:37 +0200

    added succesful test of Component class

---
+++ public/app/test/Component.test.js
@@ -0,0 +1,17 @@
+import Component from "../js/Component.js";
+
+describe("When given a Component constructor", () => {
+  describe("when recieves a parentElement 'div', a className 'class' and a 'p' tag", () => {
+    test("then it should render a 'p' element with a class named'class'", () => {
+      const parentElement = document.createElement("div");
+      const tag = "p";
+      const className = "class";
+
+      const callToFunction = new Component(parentElement, className, tag);
+      const functionAnswer = parentElement.querySelector("p.class");
+      console.log(functionAnswer);
+
+      expect(functionAnswer).not.toBeNull();
+    });
+  });
+});
