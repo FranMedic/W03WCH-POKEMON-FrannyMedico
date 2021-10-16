9aa76abd3bbbc1837b9f38bd413d8c2f3d747a76
FranMedic <m.francisca.medico@gmail.com>
2021-10-16 18:39:34 +0200

    added API url to index for calling Page component

--- public/app/js/index.js
+++ public/app/js/index.js
@@ -1,6 +1,12 @@
 import Page from "./Page.js";
+import PokemonServices from "./PokemonServices.js";
 
 const mainHtmlTag = document.querySelector("main");
 
 // eslint-disable-next-line no-unused-vars
-const webPage = new Page(mainHtmlTag);
+const webPage = new Page(
+  mainHtmlTag,
+  "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"
+);
+
+// new PokemonServices("https://pokeapi.co/api/v2/pokemon?offset=0&limit=10");
